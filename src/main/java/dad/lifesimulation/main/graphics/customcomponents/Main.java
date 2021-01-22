f807d94f9d371503c25db64985c70c7cbcd364fb
JulioCesarAM <fhoenixeterno@gmail.com>
2021-01-22 14:49:20 +0100

    cambios
    añadida doc entity ,  eliminado paquete actor.prinicpal  y renombrado paquete actor.assasin por actor.cell, clase protagonist eliminada , y añadido propertys al componente personalizado

--- src/main/java/dad/lifesimulation/main/graphics/customcomponents/Main.java
+++ src/main/java/dad/lifesimulation/main/graphics/customcomponents/Main.java
@@ -1,9 +1,23 @@
 package dad.lifesimulation.main.graphics.customcomponents;
 
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ObservableValue;
+
 public class Main{
 
 	public static void main(String[] args) {
 		App.main(args);
+	
+	
+	
+		
+	
 
 	}
-}
+
+
+
+
+}	
